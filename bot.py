7b95f2b8162f466fedd338c91971e7adf917479c
Jotunn9025 <youhanlalwani@gmail.com>
2024-03-23 15:49:57 +0530

    readfile+dicerolling

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import config
 from discord.ext import commands
 import time
+import random
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -12,7 +13,8 @@ command_line_ranges = {
     "~millet": slice(25, 43),
     "~draconis": slice(44,55),
     "~dory":slice(56,69),
-    "~help":slice(70,75),
+    "~help":slice(70,76),
+    "~r":slice(0,0),
 }
 
 last_execution_times = {}
@@ -30,17 +32,34 @@ async def on_message(message):
         if message.content.startswith(command):
             current_time = time.time()
             last_exec_time = last_execution_times.get((message.author.id, command), 0)
-            if current_time - last_exec_time > 60:
+            cooldown = 5 if command in ["~r","~help"] else 60
+            if current_time - last_exec_time > cooldown:
                 last_execution_times[(message.author.id, command)] = current_time
                 
-                peppa_str = ""
-                with open("peppa.txt", "r") as fp:
-                    lines = fp.readlines()
-                    for line in lines[line_range]:
-                        peppa_str += line
-                await message.channel.send(peppa_str)
+                if command == "~r":
+                    try:
+                        args = message.content.split()[1:]
+                        if len(args) != 1:
+                            await message.channel.send("Invalid format. Please use: ~r [number of dice]d[number of faces]")
+                            return
+                        num_dice, num_faces = map(int, args[0].split('d'))
+                        if num_dice > 20 or num_faces > 20:
+                            await message.channel.send("You can only roll up to 20 dice or 20 faces.")
+                            return
+                        rolls = [random.randint(1, num_faces) for _ in range(num_dice)]
+                        total = sum(rolls)
+                        await message.channel.send(f"Rolls: {' '.join(map(str, rolls))}\nTotal: {total}")
+                    except ValueError:
+                        await message.channel.send("Invalid format. Please use: ~r [number of dice]d[number of faces]")
+                else:
+                    peppa_str = ""
+                    with open("peppa.txt", "r") as fp:
+                        lines = fp.readlines()
+                        for line in lines[line_range]:
+                            peppa_str += line
+                    await message.channel.send(peppa_str)
             else:
-                await message.channel.send("You can only use this command once per minute.")
+                await message.channel.send(f"You can only use this command once every {cooldown} seconds.")
             break 
 
 client.run(config.DISCORD_TOKEN)
